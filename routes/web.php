3074abebe926605ebb384b8d2417fc55432b55f2
“Masato <miyamoto.tam@tam-tam.co.jp>
2019-10-24 10:17:26 +0900

    init

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Forward specific route to nuxt router
+ *
+ * This route is redered by `<nuxtRoot>/pages/index.vue`
+ */
+// Route::get(
+//     '{uri}',
+//     '\\' . Pallares\LaravelNuxt\Controllers\NuxtController::class
+// )->where('uri', '.*');
+
+Route::get(
+    '{uri}',
+    '\\' . Pallares\LaravelNuxt\Controllers\NuxtController::class
+)->where('uri', '.*');
+
