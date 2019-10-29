d84dc866d35915dce1645423a96c34fa81d5332a
m2 <m2delta29@gmail.com>
2019-10-30 00:37:29 +0900

    mod: アップロード画面微調整

--- resources/nuxt/pages/upload/simpleupload.vue
+++ resources/nuxt/pages/upload/simpleupload.vue
@@ -5,15 +5,17 @@
         <v-form ref="form" v-model="valid" lazy-validation justify="center">
           <v-row align="center" justify="center">
             <v-card class="mx-auto">
-              <v-list-item>
-                <v-list-item-action>
-                  <v-icon>mdi-upload</v-icon>
-                </v-list-item-action>
-                <v-list-item-content>
-                  <v-list-item-title class="headline">チームデータアップロード</v-list-item-title>
-                  <v-list-item-subtitle>ユーザー登録処理をせずにチームデータのアップロードが可能です。</v-list-item-subtitle>
-                </v-list-item-content>
-              </v-list-item>
+              <v-card-title class="blue">
+                <v-list-item>
+                  <v-list-item-action>
+                    <v-icon>mdi-upload</v-icon>
+                  </v-list-item-action>
+                  <v-list-item-content>
+                    <v-list-item-title class="headline white--text">チームデータアップロード</v-list-item-title>
+                    <v-list-item-subtitle class="white--text">ユーザー登録処理をせずにチームデータのアップロードが可能です。</v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-card-title>
               <v-col cols="12" md="12">
                 <v-text-field
                   v-model="name"
@@ -24,13 +26,7 @@
                 ></v-text-field>
 
                 <v-textarea v-model="email" :rules="emailRules" label="コメント" required></v-textarea>
-                <v-combobox
-                  v-model="select"
-                  :items="items"
-                  label="検索タグを設定して下さい"
-                  multiple
-                  chips
-                >
+                <v-combobox v-model="select" :items="items" label="検索タグを設定して下さい" multiple chips>
                   <template v-slot:selection="data">
                     <v-chip
                       :key="JSON.stringify(data.item)"
@@ -51,8 +47,8 @@
 
                 <v-file-input append-icon show-size counter multiple label="チームデータ"></v-file-input>
               </v-col>
-              <v-card-actions >
-                <v-btn class="primary d-flex justify-md-space-center">チームデータアップロード</v-btn>
+              <v-card-actions class="justify-center">
+                <v-btn class="primary">チームデータアップロード</v-btn>
               </v-card-actions>
             </v-card>
           </v-row>
