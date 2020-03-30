a8561b2da504e81c13d78da3d836cacfe9c03dc1
Masato Miyamoto <project-europa@excite.co.jp>
2020-03-30 12:25:50 +0900

    add: Vue Routerのfile群を移行

--- resources/ts/router.ts
+++ resources/ts/router.ts
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Top from './components/pages/Top.vue'
+import Upload from './components/pages/Upload.vue'
+import SimpleUpload from './components/pages/SimpleUpload.vue'
+import SearchTeam from './components/pages/SearchTeam.vue'
+import SearchMatch from './components/pages/SearchMatch.vue'
+import SumDLTeamData from './components/pages/SumDLTeamData.vue'
+import SumDLMatchData from './components/pages/SumDLMatchData.vue'
+import Login from './components/pages/Login.vue'
+import Register from './components/pages/Register.vue'
+import Information from './components/pages/Information.vue'
+import EventNotice from './components/pages/EventNotice.vue'
 
 Vue.use(Router)
 
@@ -11,8 +21,58 @@ const router = new Router({
       path: '/',
       name: 'Top',
       component: Top,
-    }
-  ],
+    },
+    {
+      path: '/upload',
+      name: 'Upload',
+      component: Upload,
+    },
+    {
+      path: '/simpleupload',
+      name: 'SimpleUpload',
+      component: SimpleUpload,
+    },
+    {
+      path: '/search/team',
+      name: 'SearchTeam',
+      component: SearchTeam,
+    },
+    {
+      path: '/search/match',
+      name: 'SearchMatch',
+      component: SearchMatch,
+    },
+    {
+      path: '/sumdownload/team',
+      name: 'SumDLTeamData',
+      component: SumDLTeamData,
+    },
+    {
+      path: '/sumdownload/match',
+      name: 'SumDLMatchData',
+      component: SumDLMatchData,
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register,
+    },
+    {
+      path: '/information',
+      name: 'Information',
+      component: Information,
+    },
+    {
+      path: '/eventnotice',
+      name: 'EventNotice',
+      component: EventNotice,
+    },
+  ]
 });
 
 export default router
