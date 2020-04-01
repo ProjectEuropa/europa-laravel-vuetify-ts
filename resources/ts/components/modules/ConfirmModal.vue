f0ae2378302bd2cd69a552d19a469883f127ffff
Masato Miyamoto <project-europa@excite.co.jp>
2020-04-01 13:26:13 +0900

    add: アップロード確認ダイアログ追加

---
+++ resources/ts/components/modules/ConfirmModal.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" max-width="500">
+      <v-card>
+        <v-card-title class="headline">アップロードする内容を確認してください</v-card-title>
+
+        <v-card-text class="d-flex flex-column">
+          <v-chip class="ma-2" color="blue" label text-color="white" cols="12" md="12">
+            <v-icon left>mdi-account-circle</v-icon>オーナー名:
+          </v-chip>
+          <v-chip class="ma-2" color="blue" label text-color="white" cols="12" md="12">
+            <v-icon left>mdi-comment-multiple-outline</v-icon>コメント:
+          </v-chip>
+          <v-chip class="ma-2" color="blue" label text-color="white" cols="12" md="12">
+            <v-icon left>mdi-tag-plus</v-icon>検索タグ:
+          </v-chip>
+          <v-chip class="ma-2" color="blue" label text-color="white" cols="12" md="12">
+            <v-icon left>mdi-append-icon</v-icon>アップロードファイル:
+          </v-chip>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-flex>
+            <v-btn class="text-left" color="green darken-1" text @click="close">キャンセル</v-btn>
+          </v-flex>
+
+          <v-flex class="text-xs-right">
+            <v-btn color="red darken-1" text @click="close">アップロード</v-btn>
+          </v-flex>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class ConfirmModal extends Vue {
+  dialog: boolean = false;
+
+  @Prop()
+  delObj!: string;
+  /**
+   * name
+   */
+  public open() {
+    this.dialog = true;
+  }
+  /**
+   * name
+   */
+  public close() {
+    this.dialog = false;
+  }
+}
+</script>
