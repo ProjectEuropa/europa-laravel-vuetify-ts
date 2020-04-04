e8af53c777c1d691c15e6ee78f833ed95d790543
Masato Miyamoto <project-europa@excite.co.jp>
2020-04-04 21:43:40 +0900

    add: 簡易的な検索機能を追加

---
+++ app/Http/Controllers/Api/SearchController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\File;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SearchController extends Controller
+{
+    /**
+     *
+     *
+     * @return
+     */
+    public function search(Request $request)
+    {
+        $files = File::select('id', 'upload_owner_name', 'file_name', 'file_comment', 'created_at', 'upload_user_id', 'search_tag1', 'search_tag2', 'search_tag3', 'search_tag4');
+
+        return $files->paginate(10);
+    }
+}
