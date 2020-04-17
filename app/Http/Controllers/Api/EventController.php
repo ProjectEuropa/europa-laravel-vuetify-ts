9f67156bbc6972e1023ba715d5193ba91a3c01da
kosei-program-dev <kosei.program.dev@gmail.com>
2020-04-17 14:27:32 +0900

    さらにいらないコードの追加

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -9,10 +9,7 @@ use App\Event;
 class EventController extends Controller
 {
     public function getEventData(){
-      // $data = Event::get();
       $data = Event::select('id','event_name','event_details','event_closing_day','event_reference_url')->get();
-      // $jsonData = json_decode($data);
-      // dd($data);
       return ['data' => $data];
     }
 }
