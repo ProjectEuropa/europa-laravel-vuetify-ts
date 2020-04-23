ecfceff85e49ab35b295c5524ad2cadb8ddfff8a
Masato Miyamoto <project-europa@excite.co.jp>
2020-04-23 12:58:30 +0900

    add: パスワードリセット パスワードリセット（Email送信）画面のみ作成

--- resources/ts/components/pages/PasswordReset.vue
+++ resources/ts/components/pages/PasswordReset.vue
@@ -6,11 +6,12 @@
           <v-container style class="text-xs-center">
             <v-card flat>
               <v-card-title primary-title>
-                <h4>Login</h4>
+                <h4>Password Reset</h4>
               </v-card-title>
               <ValidationObserver ref="observer">
-                <v-form method="POST" action="/login" id="login">
+                <v-form method="POST" action="/password/reset" id="password-reset">
                   <input type="hidden" name="_token" :value="csrf" />
+
                   <v-col cols="12" md="12">
                     <ValidationProvider
                       v-slot="{ errors }"
@@ -27,33 +28,45 @@
                         required
                       ></v-text-field>
                     </ValidationProvider>
-                    <ValidationProvider v-slot="{ errors }" name="パスワード" rules="required|max:100">
+                    <ValidationProvider
+                      v-slot="{ errors }"
+                      name="パスワード"
+                      rules="required|max:100|min:8|confirmed:password_confirmation"
+                    >
                       <v-text-field
                         prepend-icon="mdi-lock"
                         v-model="password"
                         name="password"
                         :counter="100"
                         :error-messages="errors"
-                        label="パスワード"
                         type="password"
+                        label="パスワード"
+                        required
+                      ></v-text-field>
+                    </ValidationProvider>
+                    <ValidationProvider
+                      v-slot="{ errors }"
+                      name="パスワード再確認"
+                      vid="password_confirmation"
+                      rules="required|max:100|min:8"
+                    >
+                      <v-text-field
+                        prepend-icon="mdi-lock"
+                        v-model="password_confirmation"
+                        :counter="100"
+                        name="password_confirmation"
+                        :error-messages="errors"
+                        type="password"
+                        label="パスワード再確認"
                         required
                       ></v-text-field>
                     </ValidationProvider>
                   </v-col>
                   <v-card-actions>
-                    <v-btn primary large block class="primary" @click="login()">Login</v-btn>
+                    <v-btn primary large block class="primary" @click="reset()">Password Reset</v-btn>
                   </v-card-actions>
                 </v-form>
               </ValidationObserver>
-              <v-col cols="12" md="12">
-                <v-card-title class="cyan" @click="twitterLogin()">
-                  <v-icon large left>mdi-twitter</v-icon>
-                  <span class="title font-weight-light">Login with Twitter</span>
-                </v-card-title>
-              </v-col>
-              <v-col cols="12" md="12">
-                <router-link to="/password/email">forget password</router-link>
-              </v-col>
             </v-card>
           </v-container>
         </v-flex>
@@ -63,33 +76,10 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component } from "vue-property-decorator";
+import { Vue, Component, Prop } from "vue-property-decorator";
 import { ValidationObserver } from "vee-validate";
 
 @Component
-export default class Login extends Vue {
-  email: string = "";
-  password: string = "";
-  csrf: string | null = document
-    .querySelector('meta[name="csrf-token"]')!
-    .getAttribute("content");
-
-  $refs!: {
-    observer: InstanceType<typeof ValidationObserver>;
-  };
-
-  public async login() {
-    const isValid = await this.$refs.observer.validate();
-    if (isValid) {
-      (<HTMLFormElement>document.querySelector("#login")).submit();
-    }
-  }
-
-  /**
-   * name
-   */
-  public twitterLogin() {
-    location.href = "/auth/twitter";
-  }
+export default class PasswordReset extends Vue {
 }
 </script>
