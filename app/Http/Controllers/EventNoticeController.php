eb50e78a2e1dd7e17373656efafd8651f4a85901
Masato Miyamoto <project-europa@excite.co.jp>
2020-04-26 12:47:31 +0900

    mod: インデント修正

--- app/Http/Controllers/EventNoticeController.php
+++ app/Http/Controllers/EventNoticeController.php
@@ -2,30 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Event;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\User;
-use App\Event;
 
 class EventNoticeController extends Controller
 {
-    public function __construct(){
-      $this->middleware('auth');
+    public function __construct()
+    {
+        $this->middleware('auth');
     }
 
-    public function store(Request $request){
-      $user = Auth::user();
-      $eventReferenceUrl = $request->eventReferenceUrl ?? null;
-      $event = new Event;
-      $event->register_user_id     = $user->id;
-      $event->event_name           = $request->eventName;
-      $event->event_details        = $request->eventDetails;
-      $event->event_reference_url  = $eventReferenceUrl;
-      $event->event_type           = $request->eventType;
-      $event->event_closing_day    = $request->eventClosingDay;
-      $event->event_displaying_day = $request->eventDisplayingDay;
-      $event->save();
+    public function store(Request $request)
+    {
+        $user = Auth::user();
+        $eventReferenceUrl = $request->eventReferenceUrl ?? null;
+        $event = new Event;
+        $event->register_user_id = $user->id;
+        $event->event_name = $request->eventName;
+        $event->event_details = $request->eventDetails;
+        $event->event_reference_url = $eventReferenceUrl;
+        $event->event_type = $request->eventType;
+        $event->event_closing_day = $request->eventClosingDay;
+        $event->event_displaying_day = $request->eventDisplayingDay;
+        $event->save();
 
-      return redirect('/eventnotice')->with('message', 'イベント告知の登録に成功しました');
+        return redirect('/eventnotice')->with('message', 'イベント告知の登録に成功しました');
     }
 }
