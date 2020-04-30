213a8b09b1389b8d6f02561ef8dec591c0039ed4
kosei-program-dev <kosei.program.dev@gmail.com>
2020-04-30 17:00:12 +0900

    interface修正、EventNoticeにdialog追加

--- resources/ts/vue-data-entity/ScheduleDataObject.ts
+++ resources/ts/vue-data-entity/ScheduleDataObject.ts
@@ -4,11 +4,13 @@ export interface ScheduleObject {
   start: string;
   end?: string;
 }
-export interface ScheduleObjectSynchronizedLaravelEvents extends ScheduleObject {
+export interface ScheduleObjectSynchronizedLaravelEvents
+  extends ScheduleObject {
   name: string;
   details?: string;
   start: string;
   end?: string;
+  url?: String;
   event_closing_day: string;
   event_name: string;
   event_details: string;
