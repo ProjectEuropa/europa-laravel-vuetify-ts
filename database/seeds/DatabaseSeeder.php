8e1fe1f5035eb6459fe76b17aedd9f7b40954e4e
Masato Miyamoto <project-europa@excite.co.jp>
2020-05-04 22:56:29 +0900

    mod: シーダーの自動実行を破棄

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(FileDummySeeder::class);
+        // $this->call(FileDummySeeder::class);
         $this->call(EventDummySeeder::class);
-        $this->call(UserSeeder::class);
+        // $this->call(UserSeeder::class);
     }
 }
